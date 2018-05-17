6eeb2577c174923aa836d3539f185d55c5874d60
brandon-fenty <brandonfenty@gmail.com>
2018-05-16 20:14:27 -0700

    removed duplicate code

--- app.js
+++ app.js
@@ -208,27 +208,6 @@ var data = {
     }]
 };
 
-new Picture ('imgs/bag.jpg', 'R2D2 Luggage', 'r2d2');
-new Picture ('imgs/banana.jpg', 'Banana Slicer', 'nana-slicer');
-new Picture ('imgs/bathroom.jpg', 'iPad TP Stand', 'tp-stand');
-new Picture ('imgs/boots.jpg', 'Rain Flops', 'rain-flops');
-new Picture ('imgs/breakfast.jpg', 'Do it all Breakfast Machine', 'breakfast-machine');
-new Picture ('imgs/bubblegum.jpg', 'Meatball Gum', 'meatball-gum');
-new Picture ('imgs/chair.jpg', 'Chair', 'idiot-chair');
-new Picture ('imgs/cthulhu.jpg', 'Cthulhu Statue', 'cthulhu');
-new Picture ('imgs/dog-duck.jpg', 'Doggo Duck Lips', 'doggo-lips');
-new Picture ('imgs/dragon.jpg', 'Dragon Meat', 'dragon-meat');
-new Picture ('imgs/pen.jpg', 'Penware Caps', 'penware');
-new Picture ('imgs/pet-sweep.jpg', 'Pet Swiffer', 'pet-swiffer');
-new Picture ('imgs/scissors.jpg', 'Pizza Scissors', 'pizza-scissors');
-new Picture ('imgs/shark.jpg', 'Shark Attack Sleeping Bag', 'shark-bag');
-new Picture ('imgs/sweep.png', 'Baby Swiffer', 'baby-swiffer');
-new Picture ('imgs/tauntaun.jpg', 'TaunTaun Blanket', 'tauntaun');
-new Picture ('imgs/unicorn.jpg', 'Unicorn Meat', 'unicorn-meat');
-new Picture ('imgs/usb.gif', 'Tentacle USB Drive', 'tentacle-drive');
-new Picture ('imgs/water-can.jpg', 'Water Can', 'water-can');
-new Picture ('imgs/wine-glass.jpg', 'Egg Glass', 'wine-glass');
-
 // =================+++++++++
 // create chart data       ||
 // rendered using chart.js ||
